29714cb7f87c6c90f38457114d55783a882f139d
Dalia-21 <dalia.sinclair@protonmail.com>
2022-12-26 22:36:23 +1100

    place route in main script for testing purposes

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask import render_template
+import os
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return "<p>Home Page</p>"
+    # I want to shift this to config at some point, maybe make it an attribute of app
+    files = os.listdir('/home/dalia/coding/weather-forecast/weather-data')
+    return render_template('index.html', files=files)
