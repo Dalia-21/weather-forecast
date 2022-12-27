a93e733a96b3d10df67374246626f3c8b6014a56
Dalia-21 <dalia.sinclair@protonmail.com>
2022-12-27 17:53:01 +1100

    move route functions to routes and import them

--- server/__init__.py
+++ server/__init__.py
@@ -1,18 +1,6 @@
 from flask import Flask
-from flask import render_template
-import os
 
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    # I want to shift this to config at some point, maybe make it an attribute of app
-    files = os.listdir('/home/dalia/coding/weather-forecast/weather-data')
-    return render_template('index.html', files=files)
-
-
-@app.route('/<file_url>')
-def view_file(file_url):
-    return '<h1>' + file_url + '</h1>'
+from server import routes
--- server/routes.py
+++ server/routes.py
@@ -8,3 +8,9 @@ def index():
     # I want to shift this to config at some point, maybe make it an attribute of app
     files = os.listdir('/home/dalia/coding/weather-forecast/weather-data')
     return render_template('index.html', files=files)
+
+
+@app.route('/<file_url>')
+def view_file(file_url):
+    # Add check that file actually exists and implement 404
+    return '<h1>' + file_url + '</h1>'
