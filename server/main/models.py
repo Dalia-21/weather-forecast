bcebd9f9324a43133bba73f0ba557aeb2f4d2d54
Dalia <dalia.sinclair@protonmail.com>
2023-02-27 21:03:39 +1100

    add code to create user table in db

--- server/main/models.py
+++ server/main/models.py
@@ -1,5 +1,4 @@
 from flask_login import UserMixin
-# from scraper.db_models import Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -11,3 +10,9 @@ class User(Base, UserMixin):
     id = Column(Integer, primary_key=True)
     username = Column(String(255))
     password_hash = Column(String(255))
+
+
+if __name__ == '__main__':
+    from scraper.db_connection import get_engine
+    engine = get_engine()
+    Base.metadata.create_all(engine)
